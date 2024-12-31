a6e5a94f642168b0c5e74e17b0e1565260e8a891
Rozir Wobari <1rozirwobari@gmail.com>
2024-12-31 12:04:32 +0700

    Add default Users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -28,14 +28,14 @@ return new class extends Migration
             $table->timestamp('created_at')->nullable();
         });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin123'),
+            'saldo' => 0,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 
     /**
